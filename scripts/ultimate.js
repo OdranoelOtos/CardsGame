7f42247ccb2fad2bcdec637ca1248815babfc4b1
Leonardo E. Soto <103690680+OdranoelOtos@users.noreply.github.com>
2022-08-05 19:22:04 -0300

    Add files via upload

--- scripts/ultimate.js
+++ scripts/ultimate.js
@@ -2,24 +2,18 @@ var playerName="";
 var url = location.href;
 var creditos = 50;
 
-//http://localhost:5500/juego/consola.html?name=leo
- 
-if (!url==""){
-    const arr_url = url.split("?");
-    if (arr_url.length>1){
-        const arr_name = arr_url[1].split("=");
-        if (arr_name.length>1){
-            
-            alert (arr_name[1]);
-            document.getElementById("titulo1").innerHTML = arr_name[1];
-            
-        }    
+function onLoading() {
+    //http://localhost:5500/juego/consola.html?name=leo
+    //action="machine"
+    if (!url==""){
+        const arr_url = url.split("?");
+        if (arr_url.length>1){
+            const arr_name = arr_url[1].split("=");
+            if (arr_name.length>1){
+                alert("aca");
+                alert(document.getElementById("titulo1"));
+                //document.getElementById("titulo1").innerHTML = arr_name[1];
+            }    
+        }
     }
 }
-
-function redirect() {
-    playerName = document.getElementById("playerName").value;
-}
-//alert (playerName);
-//onclick="redirect()" 
-
