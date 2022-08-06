789a325bbd93505db3597cf755451bf91c6ab690
Leonardo E. Soto <103690680+OdranoelOtos@users.noreply.github.com>
2022-08-06 18:30:22 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,18 @@
+//enlace con la base de datos
+var  mysql = require('mysql');
+
+var conexion = mysql.createConnection({
+    host: 'localhost',
+    database: 'blackjack',
+    user: 'root',
+    password: ""
+});
+
+conexion.connect(function(error){
+    if (error){
+        throw error;
+    }else{
+        console.log ('conexion exitosa')
+    }
+})
+conexion.end();
--- scripts/login.js
+++ scripts/login.js
@@ -1,9 +1,6 @@
-function validateForm() {
-    if (document.getElementById("playerName").value == "") {
-        alert("Please, we need a Login name!!!");
-        return false;
-    }
-    else {
-        return true;
-    }
+function onload(){
+    alert("document");
+//    alert(document.getElementById("playerName").value);
+//const name = 
+//action="consola.html" 
 }
