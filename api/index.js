af393d096fd19061393bc79790e050838829b9b2
Leonardo E. Soto <103690680+OdranoelOtos@users.noreply.github.com>
2022-08-13 10:00:57 -0300

    Cambio de lugar index.js para que sea una api y tener las consultas centralizadas ahí.

---
+++ api/index.js
@@ -0,0 +1,26 @@
+//enlace con la base de datos
+var  mysql = require('mysql');
+
+var conexion = mysql.createConnection({
+    host: 'localhost',
+    database: 'blackjack',
+    user: 'root',
+    password: ""
+});
+
+conexion.connect(function(error){
+    if (error){
+        throw error;
+    } else {
+        console.log ('conexion exitosa')
+    }
+});
+
+//Obtener Tabla jugador donde se guardó el login (nombre) y un id_jugador
+
+//Realizar la consulta a la BD y cargar el array, buscando todas las cartas posibles
+//$consulta = "SELECT * FROM mazo"
+exports.consultarMazo = consultar_mazo(conexion, "SELECT * FROM mazo");
+
+//Realizar la consulta a la BD y traer las 5 cartas mejores
+//$consulta = "SELECT TOP(5) id_jugada, valor_apuesta, nombre FROM jugada INNER JOIN jugador ON jugada.id_jugador = jugador.id_jugador"
